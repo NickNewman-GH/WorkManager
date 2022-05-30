366a58f4dff4a4f95654896963b1521fc80f20bb
Никита Трощенко <nitrix38r@gmail.com>
2022-05-31 01:07:53 +0800

    commit

--- app/src/main/java/com/example/workmanagerdemo/MainActivity.kt
+++ app/src/main/java/com/example/workmanagerdemo/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.example.workmanagerdemo
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.work.OneTimeWorkRequest
 import androidx.work.WorkManager
@@ -11,16 +13,22 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val citiesData = workDataOf("cities" to intArrayOf(123, 234, 456))
-        val workRequest = OneTimeWorkRequest.Builder(WeatherWorker::class.java)
-            .setInputData(citiesData)
-            .build()
+        val cities = arrayOf("Irkutsk", "Moscow", "Omsk")
+        findViewById<TextView>(R.id.city1).text = cities[0]
+        findViewById<TextView>(R.id.city2).text = cities[1]
+        findViewById<TextView>(R.id.city3).text = cities[2]
 
-        WorkManager.getInstance(this).enqueue(workRequest)
+        findViewById<Button>(R.id.getWeather).setOnClickListener() {
+            val citiesData = workDataOf("cities" to cities)
+            val workRequest = OneTimeWorkRequest.Builder(WeatherWorker::class.java)
+                .setInputData(citiesData)
+                .build()
+
+            WorkManager.getInstance(this).enqueue(workRequest)
+        }
 
         // TODO: реализовать запрос погоды
         // TODO: какие параметры позволяет задать WorkManager (макс число одновр заданий)
         // TODO: реализовать выполнение заданий по очереди
-
     }
 }
--- app/src/main/java/com/example/workmanagerdemo/WeatherWorker.kt
+++ app/src/main/java/com/example/workmanagerdemo/WeatherWorker.kt
@@ -2,20 +2,63 @@ package com.example.workmanagerdemo
 
 import android.content.Context
 import android.util.Log
+import android.widget.EditText
 import androidx.work.Worker
 import androidx.work.WorkerParameters
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
+import org.json.JSONObject
+import java.io.FileNotFoundException
+import java.io.InputStream
+import java.net.URL
+import java.net.UnknownHostException
+import java.util.*
+import kotlin.collections.HashMap
 
 class WeatherWorker(context: Context, workerParams: WorkerParameters) :
     Worker(context, workerParams)
 {
     override fun doWork(): Result {
-        val cityIDs = inputData.getIntArray("cities")
-        
-        cityIDs?.let {
-            for (c in it)
-                Log.d("mytag", "city $c")
+        val cities = inputData.getStringArray("cities")
+
+        cities?.let {
+            for (city in it) {
+                GlobalScope.launch (Dispatchers.IO) {
+                    loadWeather(city)
+                }
+                Log.d("mytag", "city $city")
+            }
         }
         Log.d("mytag", "work success")
         return Result.success()
     }
+
+
+    suspend fun loadWeather(city: String): String {
+        val API_KEY = "d6843ab8ee963f5d372296dfff62aed7"
+        //val editText = findViewById<EditText>(R.id.city)
+
+        val weatherURL =
+            "https://api.openweathermap.org/data/2.5/weather?q=$city&appid=$API_KEY&units=metric";
+        try{
+            val stream = URL(weatherURL).getContent() as InputStream
+            val data = Scanner(stream).nextLine()
+            Log.d("mytag", data)
+            val jObject = JSONObject(data)
+            val sky = jObject.getJSONArray("weather").getJSONObject(0)["main"].toString()
+            val temp = jObject.getJSONObject("main")["temp"].toString() + " °C"
+            //Log.d("mytag", sky)
+            val wind = jObject.getJSONObject("wind")
+            val windSpeed = wind["speed"].toString() + " m/s"
+            return "Weather is $sky, temperature = $temp, wind speed = $windSpeed"
+            //Log.d("mytag", wind.toString())
+        } finally {
+
+        }
+    }
 }
+
+data class WeatherInfoFirst(val message:String = "")
+data class WeatherInfoSecond(val message:String = "")
+data class WeatherInfoThird(val message:String = "")
